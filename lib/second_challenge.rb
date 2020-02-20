2525f47d8cec1dffed141f7cd917277db0d918c5
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-20 20:41:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       #your key/value pairs here!
 
       },
-    :capulet => {:patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => {}
+    :capulet => {:patriarch => {}, :matriarch => {}, :hero => {}, :heroine_friends => {}
       #your key/value pairs here!
       }}
 
